5b5a779ef1afd2634c979268b091a81138338f0d
Felipebyjus1 <125500685+Felipebyjus1@users.noreply.github.com>
2023-08-02 15:05:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+// https://teachablemachine.withgoogle.com/models/L4Gid02ZG/
+
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: "png",
+    png_quality: 90
+    })
+    
+    var camera = document.getElementById("camera")
+    
+    Webcam.attach(camera)
+    
+    function tirarFoto(){
+        Webcam.snap(function(data_uri){
+            document.getElementById("resulatdo").innerHTML = `<img id="foto" src="${data_uri}" /> `
+        })
+    }
+    
+    console.log("versão ml5", ml5.version )
+    
+    var classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/L4Gid02ZG/model.json", carregarmodelo)
+    
+    function carregarmodelo(){
+        console.log("modeloCarregado")
+    }
+    
+    function checar(){
+        var img = document.getElementById("foto")
+        classifier.classify(img, pegarResultados)
+    }
+    
+    function pegarResultados(error, result){
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log(result)
+    }
+    
+    }
